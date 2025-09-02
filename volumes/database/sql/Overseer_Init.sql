2ecacb1a3b1904fe17d20293e52c5d96dd3e93e7
Luke Park <luke.matthew.park@hotmail.co.uk>
2025-09-02 21:25:55 +0100

    Table creator

--- volumes/database/sql/Overseer_Init.sql
+++ volumes/database/sql/Overseer_Init.sql
@@ -1,1 +1,23 @@
-USE Overseer;
+
+/* Create the users table */
+
+CREATE TABLE Overseer_users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    permissions VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    remember_token CHAR(64),
+    remember_token_expiration DATETIME,
+    devices INT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+/* Create the device table */
+
+CREATE TABLE Overseer_devices (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    device_name VARCHAR(255),
+    parent INT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (parent) REFERENCES Overseer_users(id)
+);
