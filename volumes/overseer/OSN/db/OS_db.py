dac309971e44881dc0d6953f3b4fc93c3ebbc667
Luke Park <luke.matthew.park@hotmail.co.uk>
2025-09-30 12:21:07 +0100

    Altered ssl and pool to class level variables

--- volumes/overseer/OSN/db/OS_db.py
+++ volumes/overseer/OSN/db/OS_db.py
@@ -12,9 +12,9 @@ def async_db_operation (method) :
         Custom decorator used to execute async operations on the database.
     """
     async def db_op (self, *args, **kwargs) :
-
+        # Use the connection pool within OS_db to execute queries.
         try:
-            async with self.pool.acquire() as conn:
+            async with OS_db.pool.acquire() as conn:
                 async with conn.cursor() as cursor:
 
                     # The method needs to have the following arguments: self, cursor
@@ -29,47 +29,54 @@ def async_db_operation (method) :
 
 class OS_db:
 
+    pool = None # Pools Closed...
+    _pool_lock = asyncio.Lock() 
+
+    # Create the SSL context.
+    ssl_ctx = ssl.create_default_context(cafile="/certs/SSL-root.crt")
+    ssl_ctx.load_cert_chain(certfile="/certs/overseer.crt", keyfile="/certs/overseer.key")
+
+
     def __init__ (self) :
         # Initialise the database connection.
-
-        self.pool = None
 
         self.host     = os.getenv('DB_HOST')
         self.user     = os.getenv('DB_NAME')
         self.password = os.getenv('MASTER_PASSWORD')
         self.db       = os.getenv('DB_NAME')
 
-        # Create SSL context
-        self.ssl_ctx = ssl.create_default_context(cafile="/certs/SSL-root.crt")
-        self.ssl_ctx.load_cert_chain(certfile="/certs/overseer.crt", keyfile="/certs/overseer.key")
-
 
     async def init_connection (self) :
         # Initialise the async connection to the database
-        while not self.pool:
+        while not OS_db.pool:
+            # Acquire the lock so that only one instance of the pool is created.
+            # Very rare that two instances would create at the same time but whatever... its done now.
+            async with OS_db._pool_lock:
+                if OS_db.pool is None:
         
-            try:
-                # Create a connection pool.
-                # This way we dont have to remake the connection everytime.
-                self.pool = await aiomysql.create_pool(
-                    host=self.host,
-                    user=self.user,
-                    password=self.password,
-                    db=self.db,
-                    autocommit=True,
-                    ssl=self.ssl_ctx
-                )
-                print(f"OS_db connected to the Overseer database @ {self.host}")
+                    try:
+                        # Create a connection pool.
+                        # This way we dont have to remake the connection everytime.
+                        OS_db.pool = await aiomysql.create_pool(
+                            host=self.host,
+                            user=self.user,
+                            password=self.password,
+                            db=self.db,
+                            autocommit=True,
+                            ssl=OS_db.ssl_ctx
+                        )
+                        print(f"OS_db connected to the Overseer database @ {self.host}")
 
-            except Exception as e:
-                print(f"Failed to connect to Overseer database @ {self.host}")
-                await asyncio.sleep(1)
+                    except Exception as e:
+                        print(f"Failed to connect to Overseer database @ {self.host}")
+                        await asyncio.sleep(1)
 
         # Generate a password hash and a secret key from the master password.
         hash, key = OS_db.hash_pwd(self.password)
 
         # Generate the admin user if it hasnt been created.
         await self.call_procedure("create_user", f"{self.db}_admin", "*", hash, key)
+
 
     @staticmethod
     def hash_pwd (password) : 
@@ -81,13 +88,14 @@ class OS_db:
         secret_key = ''.join(secrets.choice(alphabet) for _ in range(64))
 
         return (password, secret_key)
+    
 
-
-    async def close (self) :
+    @classmethod
+    async def close (cls) :
         # Close the connection to the database.
-        if (self.pool) :
-            self.pool.close()
-            await self.pool.wait_closed()
+        if (cls.pool) :
+            cls.pool.close()
+            await cls.pool.wait_closed()
             print("Closed connection to the database.")
 
 
