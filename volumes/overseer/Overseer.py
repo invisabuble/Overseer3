1a5d15c9ac6c81731939185046542f38a19e475e
Luke Park <luke.matthew.park@hotmail.co.uk>
2026-07-08 20:52:50 +0100

    Cotroutine conflict fix

--- volumes/overseer/Overseer.py
+++ volumes/overseer/Overseer.py
@@ -1,11 +1,4 @@
 from OSN.OSS import *
 
 OSN = OSS()
-
-async def main () :
-    # Main loop for overseer.
-    await OSN.run()
-
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-loop.run_until_complete(main())
+OSN.run()
