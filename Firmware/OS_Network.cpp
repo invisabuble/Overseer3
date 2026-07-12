e0403513ac8d9ea70b8a0478299b3fd56d7909b1
Luke Park <luke.matthew.park@hotmail.co.uk>
2026-07-12 21:59:39 +0100

    Websocket stuff

--- Firmware/OS_Network.cpp
+++ Firmware/OS_Network.cpp
@@ -15,9 +15,13 @@ void OS_Network::Init () {
     // Get a pointer to the config object.
     OS_Config& cnf = OS_Config::inst();
 
+    // Setup strings to copy the variables into.
+    String ssid = cnf["SSID"];
+    String pswd = cnf["PSWD"];
+
     // Initiate the WiFi connection.
     WiFi.mode(WIFI_STA);
-    WiFi.begin(cnf["SSID"].c_str(), cnf["PSWD"].c_str());
+    WiFi.begin(ssid.c_str(), pswd.c_str());
 
     // Pasue here until  WiFi connects. Blink the status light to show connection attempt in progress.
     while (WiFi.status() != WL_CONNECTED) {
@@ -27,4 +31,44 @@ void OS_Network::Init () {
 
     //Drive the LED low again when the connection attempt has succeeded.
     digitalWrite(STATUS_LED, LOW);
+
+    // Setup strings to hold the websocket information.
+    String host = cnf["HOST"];
+    String port = cnf["PORT"];
+    String cert = cnf["__CERT__"];
+
+    // Initialise the websocket connection.
+    websocket.beginSslWithCA(
+        host.c_str(),
+        port.toInt(),
+        "/",
+        cert.c_str()
+    );
+    websocket.onEvent(websocket_event);
+    websocket.setReconnectInterval(5000);
+}
+
+void OS_Network::websocket_event(WStype_t type, uint8_t* payload, size_t length) {
+    switch (type) {
+        case WStype_CONNECTED:
+            Serial.println("WSS connected");
+            break;
+        case WStype_DISCONNECTED:
+            Serial.println("WSS disconnected");
+            break;
+        case WStype_TEXT:
+            Serial.printf("WSS received: %s\n", payload);
+            // This is where an incoming config push would get routed to
+            // OS_Config::inst().write_new_config(...), once you build that handler.
+            break;
+        case WStype_ERROR:
+            Serial.println("WSS error");
+            break;
+        default:
+            break;
+    }
+}
+
+void OS_Network::update () {
+    websocket.loop();
 }
--- Firmware/OS_Network.h
+++ Firmware/OS_Network.h
@@ -2,14 +2,20 @@
 #define OS_NETWORK_H
 
 #include <WiFi.h>
+#include <WebSocketsClient.h>
 
 class OS_Network {
   // Encapsulate the network and websocket connections.
   private:
     OS_Network();
+    static void websocket_event (WStype_t type, uint8_t* payload, size_t length);
+
+    WebSocketsClient websocket;
+
   public:
     static OS_Network& inst();
     void Init();
+    void update();
 };
 
 #endif
