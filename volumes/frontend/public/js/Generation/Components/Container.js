97f908484cd52e6a6b363d061da02071ae13f556
Luke Park <luke.matthew.park@hotmail.co.uk>
2026-07-15 17:43:52 +0100

    container animation close

--- volumes/frontend/public/js/Generation/Components/Container.js
+++ volumes/frontend/public/js/Generation/Components/Container.js
@@ -319,7 +319,7 @@ export default class Container extends Generic_Commander {
     close () {
         console.log(`Closing ${this.UUID}`);
         const parent_container = this.COM.container;
-        parent_container.style.animation = "fade_in 0.5s ease backwards";
+        parent_container.style.animation = "fade_out 0.5s ease forwards";
         delete window.Controllables[this.UUID];
         setTimeout(() => {parent_container .remove()}, 500);
     }
